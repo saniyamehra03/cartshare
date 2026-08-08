a599dc6ff51520f842856c5bf95390071edfa2e6
saniya mehra <mehrasaniya868@gmail.com>
2026-08-08 11:57:45 +0530

    Connected cart form inputs with JavaScript

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,16 @@
+const productInput = document.getElementById("product");
+const quantityInput = document.getElementById("quantity");
+const priceInput = document.getElementById("price");
 const addProductBtn = document.getElementById("addProductBtn");
 console.log(addProductBtn);
+ 
+addProductBtn.addEventListener("click" , function(){
+
+    const product= productInput.value.trim();
+    const quantity = quantityInput.value.trim();
+    const price = priceInput.value.trim();
+
+    console.log(product);
+    console.log(quantity);
+    console.log(price);
+});
